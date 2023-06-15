acef708ed27186ec4c032568301522a9f2912a85
amirmtolouie@gmail.com <amirmtolouie@gmail.com>
2023-06-15 11:30:01 +0300

    Finished

--- PerfectPay/MainPage.xaml.cs
+++ PerfectPay/MainPage.xaml.cs
@@ -2,23 +2,84 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    public int Tip { get; set; }
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public decimal Bill { get; set; }
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    public int PersonCount { get; set; } = 1;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    private void txtBill_Completed(object sender, EventArgs e)
+    {
+        Bill = Convert.ToDecimal(txtBill.Text);
+        CalculateTotal();
+    }
+
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        if (sender is Button)
+        {
+
+            var btn = (Button)sender;
+
+            var percentage = btn.Text.Replace("%", "");
+
+            sldTip.Value = int.Parse(percentage);
+        }
+    }
+
+
+    private void sldTip_ValueChanged(object sender, ValueChangedEventArgs e)
+    {
+        Tip = Convert.ToInt32(sldTip.Value);
+        lblTip.Text = $"Tip : {Tip}%";
+        CalculateTotal();
+    }
+
+    private void btnMinus_Clicked(object sender, EventArgs e)
+    {
+        if (PersonCount > 1)
+        {
+            PersonCount--;
+        }
+        
+        lblNoPerons.Text = PersonCount.ToString();
+
+        CalculateTotal();
+
+    }
+
+    private void btnPlus_Clicked(object sender, EventArgs e)
+    {
+        
+        PersonCount++;
+        
+        lblNoPerons.Text = PersonCount.ToString();
+
+        CalculateTotal();
+    }
+    private void CalculateTotal()
+    {
+        var totalTip = (Bill * Tip) / 100;
+
+        var topPerPerson = totalTip / PersonCount;
+
+        lblTipByPerson.Text = $"{topPerPerson:C}";
+
+        var subTotal = Bill / PersonCount;
+
+        lblSubtotal.Text = $"{subTotal:C}";
+
+        totalTip = (Bill + totalTip) / PersonCount;
+
+        lblTotal.Text = $"{totalTip:C}";
+
+    }
+
 }
 
